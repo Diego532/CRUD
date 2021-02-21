b3cde096c6fe5ee19493efb25d534cacacd3421b
Diego <diegobastardo1@gmail.com>
2021-02-20 21:43:37 -0400

    Conector listo, se agrego el paquete formulario

--- Crud_escuela/src/conexionSQL/conexionSQL.java
+++ Crud_escuela/src/conexionSQL/conexionSQL.java
@@ -1,7 +1,29 @@
 //Aqui es donde vamos a revisar nuestra conexion 
 package conexionSQL;
 
+import com.mysql.jdbc.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
 
 public class conexionSQL {
+    
+    Connection conectar = null;
+    
+    public Connection conexion()
+    {
+        
+        try
+        {
+            Class.forName("com.mysql.jdbc.Driver");
+            conectar = (Connection) DriverManager.getConnection("jdbc:mysql://localhost/escuela","root",""); //Enlace a la base de datos 
+            
+            JOptionPane.showMessageDialog(null,"Conexion Exitosa"); // esto s ehace para si la conexion es exitosa muestre un mensjae en pantalla
+            
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null,"Error de Conexion" + e.getMessage());
+        }
+        
+        return conectar;
+    }
     
 }
